aa2a20cdc5fed4f36c399a0e7d55a4894fefcba2
RaMadaSilva <raul01mateia@gmail.com>
2023-02-06 14:51:17 +0100

    inserirndo uma connections string tests

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -5,12 +5,15 @@ namespace ApiCliente.Data
 {
     public class ApiDbContext : DbContext
     {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
         public DbSet<Cliente> Clientes { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source= .apiCliente.db;Cache=Shared");
-        }
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseSqlite("Data Source= .apiCliente.db;Cache=Shared");
+        // }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using ApiCliente.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionsString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ApiDbContext>();
+builder.Services.AddDbContext<ApiDbContext>(options => options.UseSqlite(connectionsString));
 
 var app = builder.Build();
 app.MapControllers();
